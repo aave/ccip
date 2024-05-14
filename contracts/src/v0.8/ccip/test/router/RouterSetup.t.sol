305fc884a229058f7fa8a232a5ecc310dc535e34
miguelmtzinf <miguelmtz.mail@gmail.com>
2024-05-14 17:52:28 +0200

    test: Add unit and e2e tests

--- contracts/src/v0.8/ccip/test/router/RouterSetup.t.sol
+++ contracts/src/v0.8/ccip/test/router/RouterSetup.t.sol
@@ -1,6 +1,10 @@
 // SPDX-License-Identifier: BUSL-1.1
 pragma solidity 0.8.19;
 
+import {TransparentUpgradeableProxy} from "solidity-utils/contracts/transparent-proxy/TransparentUpgradeableProxy.sol";
+import {UpgradeableLockReleaseTokenPool} from "../../pools/UpgradeableLockReleaseTokenPool.sol";
+import {UpgradeableBurnMintTokenPool} from "../../pools/UpgradeableBurnMintTokenPool.sol";
+
 import {BaseTest} from "../BaseTest.t.sol";
 import {Router} from "../../Router.sol";
 import {WETH9} from "../WETH9.sol";
@@ -35,5 +39,85 @@ contract RouterSetup is BaseTest {
         data: bytes("a"),
         destTokenAmounts: ta
       });
+  }
+
+  function _deployUpgradeableBurnMintTokenPool(
+    address ghoToken,
+    address arm,
+    address router,
+    address owner,
+    address proxyAdmin
+  ) internal returns (address) {
+    // Deploy BurnMintTokenPool for GHO token on source chain
+    UpgradeableBurnMintTokenPool tokenPoolImpl = new UpgradeableBurnMintTokenPool(ghoToken, arm, false);
+    // Imple init
+    address[] memory emptyArray = new address[](0);
+    tokenPoolImpl.initialize(owner, emptyArray, router);
+    // proxy deploy and init
+    bytes memory tokenPoolInitParams = abi.encodeWithSignature(
+      "initialize(address,address[],address)",
+      owner,
+      emptyArray,
+      router
+    );
+    TransparentUpgradeableProxy tokenPoolProxy = new TransparentUpgradeableProxy(
+      address(tokenPoolImpl),
+      proxyAdmin,
+      tokenPoolInitParams
+    );
+    // Manage ownership
+    vm.stopPrank();
+    vm.prank(owner);
+    UpgradeableBurnMintTokenPool(address(tokenPoolProxy)).acceptOwnership();
+    vm.startPrank(OWNER);
+
+    return address(tokenPoolProxy);
+  }
+
+  function _deployUpgradeableLockReleaseTokenPool(
+    address ghoToken,
+    address arm,
+    address router,
+    address owner,
+    uint256 bridgeLimit,
+    address proxyAdmin
+  ) internal returns (address) {
+    UpgradeableLockReleaseTokenPool tokenPoolImpl = new UpgradeableLockReleaseTokenPool(ghoToken, arm, false, true);
+    // Imple init
+    address[] memory emptyArray = new address[](0);
+    tokenPoolImpl.initialize(owner, emptyArray, router, bridgeLimit);
+    // proxy deploy and init
+    bytes memory tokenPoolInitParams = abi.encodeWithSignature(
+      "initialize(address,address[],address,uint256)",
+      owner,
+      emptyArray,
+      router,
+      bridgeLimit
+    );
+    TransparentUpgradeableProxy tokenPoolProxy = new TransparentUpgradeableProxy(
+      address(tokenPoolImpl),
+      proxyAdmin,
+      tokenPoolInitParams
+    );
+
+    // Manage ownership
+    vm.stopPrank();
+    vm.prank(owner);
+    UpgradeableLockReleaseTokenPool(address(tokenPoolProxy)).acceptOwnership();
+    vm.startPrank(OWNER);
+
+    return address(tokenPoolProxy);
+  }
+
+  function _getProxyAdminAddress(address proxy) internal view returns (address) {
+    bytes32 ERC1967_ADMIN_SLOT = 0xb53127684a568b3173ae13b9f8a6016e243e63b6e8ee1178d6a717850b5d6103;
+    bytes32 adminSlot = vm.load(proxy, ERC1967_ADMIN_SLOT);
+    return address(uint160(uint256(adminSlot)));
+  }
+
+  function _getProxyImplementationAddress(address proxy) internal view returns (address) {
+    bytes32 ERC1967_IMPLEMENTATION_SLOT = 0x360894a13ba1a3210667c828492db98dca3e2076cc3735a920a3ca505d382bbc;
+    bytes32 implSlot = vm.load(proxy, ERC1967_IMPLEMENTATION_SLOT);
+    return address(uint160(uint256(implSlot)));
   }
 }
