459ffe869e8482a949577afe9adaaf212a143ce3
DhairyaSethi <55102840+DhairyaSethi@users.noreply.github.com>
2024-10-18 18:00:50 +0530

    test: fork test pre migration setup

--- contracts/src/v0.8/ccip/test/pools/GHO/fork/GhoTokenPoolMigrate1_4To1_5/ForkBase.t.sol
+++ contracts/src/v0.8/ccip/test/pools/GHO/fork/GhoTokenPoolMigrate1_4To1_5/ForkBase.t.sol
@@ -14,8 +14,13 @@ import {UpgradeableLockReleaseTokenPool_Sepolia} from "./LegacyTestnetTokenPools
 import {UpgradeableBurnMintTokenPool_ArbSepolia} from "./LegacyTestnetTokenPools/UpgradeableBurnMintTokenPool_ArbSepolia.sol";
 
 interface IRouter is IRouterClient, IRouterBase {
+  struct OffRamp {
+    uint64 sourceChainSelector;
+    address offRamp;
+  }
   function getWrappedNative() external view returns (address);
   function isOffRamp(uint64, address) external view returns (bool);
+  function getOffRamps() external view returns (OffRamp[] memory);
 }
 
 struct SourceTokenData {
@@ -61,9 +66,12 @@ contract ForkBase is Test {
 
   address internal alice = makeAddr("alice");
 
+  uint256 internal constant BLOCK_AFTER_MIGRATION_L1 = 6884195;
+  uint256 internal constant BLOCK_AFTER_MIGRATION_L2 = 89058935;
+
   function setUp() public virtual {
-    l1.forkId = vm.createFork("https://sepolia.gateway.tenderly.co", 6884195);
-    l2.forkId = vm.createFork("https://arbitrum-sepolia.gateway.tenderly.co", 89058935);
+    l1.forkId = vm.createFork(vm.rpcUrl("sepolia"), BLOCK_AFTER_MIGRATION_L1);
+    l2.forkId = vm.createFork(vm.rpcUrl("arb_sepolia"), BLOCK_AFTER_MIGRATION_L2);
 
     vm.selectFork(l1.forkId);
     l1.tokenPool = UpgradeableLockReleaseTokenPool_Sepolia(0x7768248E1Ff75612c18324bad06bb393c1206980);
@@ -130,7 +138,7 @@ contract ForkBase is Test {
   function _generateMessage(
     address receiver,
     uint256 tokenAmountsLength
-  ) internal view returns (Client.EVM2AnyMessage memory) {
+  ) internal pure returns (Client.EVM2AnyMessage memory) {
     return
       Client.EVM2AnyMessage({
         receiver: abi.encode(receiver),
@@ -222,7 +230,7 @@ contract ForkPoolAfterMigration is ForkBase {
   /// Only lockOrBurn is incompatible post migration since the new proxyPool becomes a 'wrapped' router
   /// for the existing token pool, releaseOrMint is still compatible with legacy on-ramps
   /// see more: https://github.com/smartcontractkit/ccip/blob/11c275959902783a3c4eaddbfaa5ce5f8707e01f/contracts/src/v0.8/ccip/test/legacy/TokenPoolAndProxy.t.sol#L130-L192
-  function testSendViaLegacyPoolReverts() public {
+  function testSendViaLegacyRouterReverts() public {
     uint256 amount = 10e18;
     // generate lockOrBurn message for lockRelease token pool on L1
     Client.EVM2AnyMessage memory message = _generateMessage(alice, 1);
@@ -245,5 +253,137 @@ contract ForkPoolAfterMigration is ForkBase {
     vm.prank(alice);
     vm.expectRevert(abi.encodeWithSelector(CallerIsNotARampOnRouter.selector, l2.proxyPool));
     l2.router.ccipSend{value: feeTokenAmount}(l1.chainSelector, message);
+  }
+}
+
+contract ForkPoolBeforeMigration is ForkBase {
+  uint256 internal constant BLOCK_BEFORE_MIGRATION_L1 = 6673089;
+  uint256 internal constant BLOCK_BEFORE_MIGRATION_L2 = 79570677;
+
+  function setUp() public override {
+    l1.forkId = vm.createFork(vm.rpcUrl("sepolia"), BLOCK_BEFORE_MIGRATION_L1);
+    l2.forkId = vm.createFork(vm.rpcUrl("arb_sepolia"), BLOCK_BEFORE_MIGRATION_L2);
+
+    vm.selectFork(l1.forkId);
+    l1.tokenPool = UpgradeableLockReleaseTokenPool_Sepolia(0x7768248E1Ff75612c18324bad06bb393c1206980);
+    l1.router = IRouter(l1.tokenPool.getRouter());
+    l2.chainSelector = l1.tokenPool.getSupportedChains()[0];
+    l1.token = l1.tokenPool.getToken();
+    l1.EVM2EVMOnRamp1_2 = IEVM2AnyOnRamp(l1.router.getOnRamp(l2.chainSelector));
+    l1.EVM2EVMOffRamp1_2 = IAny2EVMOffRamp(0xdb92e73d1D630B5B7aC96840c4df0c591c7Ad23E);
+    vm.prank(alice);
+    l1.token.approve(address(l1.router), type(uint256).max);
+    deal(address(l1.token), alice, 1000e18);
+    deal(alice, 1000e18);
+
+    vm.selectFork(l2.forkId);
+    l2.tokenPool = UpgradeableBurnMintTokenPool_ArbSepolia(0x3eC2b6F818B72442fc36561e9F930DD2b60957D2);
+    l2.router = IRouter(l2.tokenPool.getRouter());
+    l1.chainSelector = l2.tokenPool.getSupportedChains()[0];
+    l2.token = l2.tokenPool.getToken();
+    l2.EVM2EVMOnRamp1_2 = IEVM2AnyOnRamp(l2.router.getOnRamp(l1.chainSelector));
+    l2.EVM2EVMOffRamp1_2 = IAny2EVMOffRamp(0xFf5e1c597c5DFfC896Ab8c7b9d876D513518c4b7);
+    vm.prank(alice);
+    l2.token.approve(address(l2.router), type(uint256).max);
+    deal(address(l2.token), alice, 1000e18);
+    deal(alice, 1000e18);
+
+    vm.selectFork(l1.forkId);
+    assertEq(l1.chainSelector, 16015286601757825753);
+    assertEq(address(l1.token), 0xc4bF5CbDaBE595361438F8c6a187bDc330539c60);
+    assertEq(ITypeAndVersion(address(l1.router)).typeAndVersion(), "Router 1.2.0");
+    assertEq(ITypeAndVersion(address(l1.EVM2EVMOnRamp1_2)).typeAndVersion(), "EVM2EVMOnRamp 1.2.0");
+    assertEq(ITypeAndVersion(address(l1.EVM2EVMOffRamp1_2)).typeAndVersion(), "EVM2EVMOffRamp 1.2.0");
+    assertTrue(l1.router.isOffRamp(l2.chainSelector, address(l1.EVM2EVMOffRamp1_2)));
+    // assert only one off ramp is set
+    IRouter.OffRamp[] memory offRamps = l1.router.getOffRamps();
+    for (uint256 i; i < offRamps.length; ++i) {
+      if (offRamps[i].sourceChainSelector == l2.chainSelector) {
+        assertEq(address(l1.EVM2EVMOffRamp1_2), offRamps[i].offRamp);
+      }
+    }
+
+    vm.selectFork(l2.forkId);
+    assertEq(l2.chainSelector, 3478487238524512106);
+    assertEq(address(l2.token), 0xb13Cfa6f8B2Eed2C37fB00fF0c1A59807C585810);
+    assertEq(ITypeAndVersion(address(l2.router)).typeAndVersion(), "Router 1.2.0");
+    assertEq(ITypeAndVersion(address(l2.EVM2EVMOnRamp1_2)).typeAndVersion(), "EVM2EVMOnRamp 1.2.0");
+    assertEq(ITypeAndVersion(address(l2.EVM2EVMOffRamp1_2)).typeAndVersion(), "EVM2EVMOffRamp 1.2.0");
+    assertTrue(l2.router.isOffRamp(l1.chainSelector, address(l2.EVM2EVMOffRamp1_2)));
+    // assert only one off ramp is set
+    offRamps = l2.router.getOffRamps();
+    for (uint256 i; i < offRamps.length; ++i) {
+      if (offRamps[i].sourceChainSelector == l1.chainSelector) {
+        assertEq(address(l2.EVM2EVMOffRamp1_2), offRamps[i].offRamp);
+      }
+    }
+  }
+
+  function testSendViaRouter() public {
+    uint256 amount = 10e18;
+
+    Client.EVM2AnyMessage memory message = _generateMessage(alice, 1);
+    message.tokenAmounts[0] = Client.EVMTokenAmount({token: address(l1.token), amount: amount});
+
+    {
+      vm.selectFork(l1.forkId);
+
+      uint256 feeTokenAmount = l1.router.getFee(l2.chainSelector, message);
+
+      vm.prank(alice);
+      l1.router.ccipSend{value: feeTokenAmount}(l2.chainSelector, message);
+    }
+    {
+      vm.selectFork(l2.forkId);
+
+      message.tokenAmounts[0].token = address(l2.token);
+      uint256 feeTokenAmount = l2.router.getFee(l1.chainSelector, message);
+
+      vm.prank(alice);
+      l2.router.ccipSend{value: feeTokenAmount}(l1.chainSelector, message);
+    }
+  }
+
+  function testLockOrBurnVia1_2OnRamp() public {
+    uint256 amount = 10e18;
+    {
+      vm.selectFork(l1.forkId);
+
+      vm.prank(address(l1.EVM2EVMOnRamp1_2));
+      l1.tokenPool.lockOrBurn(alice, abi.encode(alice), amount, l2.chainSelector, "");
+    }
+    {
+      vm.selectFork(l2.forkId);
+
+      // router is responsible for transferring liquidity, so we mock router.token.transferFrom(user, tokenPool)
+      deal(address(l2.token), address(l2.tokenPool), amount);
+
+      vm.prank(address(l2.EVM2EVMOnRamp1_2));
+      l2.tokenPool.lockOrBurn(alice, abi.encode(alice), amount, l1.chainSelector, "");
+    }
+  }
+
+  function testReleaseOrMintVia1_2OffRamp() public {
+    uint256 amount = 10e18;
+    {
+      vm.selectFork(l1.forkId);
+
+      uint256 balanceBefore = l1.token.balanceOf(alice);
+
+      vm.prank(address(l1.EVM2EVMOffRamp1_2));
+      l1.tokenPool.releaseOrMint(abi.encode(alice), alice, amount, l2.chainSelector, "");
+
+      assertEq(l1.token.balanceOf(alice), balanceBefore + amount);
+    }
+    {
+      vm.selectFork(l2.forkId);
+
+      uint256 balanceBefore = l2.token.balanceOf(alice);
+
+      vm.prank(address(l2.EVM2EVMOffRamp1_2));
+      l2.tokenPool.releaseOrMint(abi.encode(alice), alice, amount, l1.chainSelector, "");
+
+      assertEq(l2.token.balanceOf(alice), balanceBefore + amount);
+    }
   }
 }
